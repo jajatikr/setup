2d27ac6c2735ef97a8cd1dbc7aea97ba47f82bd2
Jajati Keshari Routray <jajatikr@gmail.com>
2017-05-10 12:29:37 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
+#!/usr/bin/env bash
+
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo add-apt-repository ppa:webupd8team/tor-browser
 sudo apt-get update
 xargs sudo apt-get -y install < apt_requirements.txt
 sudo dpkg -i google-chrome-*.deb
 sudo apt-get -fy install
+sudo dpkg -i google-chrome-*.deb
+sudo chmod +x jetbrains_setup.sh
+./jetbrains_setup.sh
+IU
+./jetbrains_setup.sh
+A
+./jetbrains_setup.sh
+PP
+
+
 
 # Remove tor browser
 # sudo apt-get remove tor-browser
