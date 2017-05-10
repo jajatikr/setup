f35e1d310e302ab5298557bd7e3a9024956c13be
Jajati Keshari Routray <jajatikr@gmail.com>
2017-05-10 11:44:51 -0700

    Added Python-requirements and Jetbrains setup

---
+++ jetbrains_setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Install IntelliJ IDEA
+
+install() {
+
+    echo Extracting $2
+    if [ $1 == "tar" ]; then
+        sudo tar -xzf $2 -C /opt/
+    elif [ $1 == "zip" ]; then
+        sudo unzip -q $2 -d /opt/
+    fi
+    echo "Extraction completed"
+
+    echo "Installing..."
+    cd /opt/$3/bin
+    ./$4
+    echo "Installation completed"
+    exit
+
+}
+
+edition=$(echo 'Z')
+
+until [[ $edition == "IU" || $edition == "IC" || $edition == "A" || $edition == "PU" || $edition == "PC" ]]; do
+    echo -n "Install IntelliJ IDEA or Pycharm (Ultimate/Community edition) or Android-Studio [IU/IC/PU/PC/A] : "
+    read edition
+done
+
+
+if [ $edition == "IU" ]; then
+    install "tar" "ideaIU-*.tar.gz" "idea-IU-*" "idea.sh"
+elif [ $edition == "IC" ]; then
+    install "tar" "ideaIC-*.tar.gz" "idea-IC-*" "idea.sh"
+elif [ $edition == "A" ]; then
+    sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 unzip
+    install "zip" "android-*.zip" "android-*" "studio.sh"
+elif [ $edition == "PU" ]; then
+    install "tar" "pycharm-professional-*.tar.gz" "pycharm-*" "pycharm.sh"
+elif [ $edition == "PC" ]; then
+    install "tar" "pycharm-community-*.tar.gz" "pycharm-community-*" "pycharm.sh"
+fi
+
+# Uninstall
+# sudo rm -r .IntelliJIdea2017.1/
+# sudo rm -r /opt/idea-IU-171.4249.39/
